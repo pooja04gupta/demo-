690cd5e8861b0c693f15b4e12a5c2005040b1dd0
pooja <poojaguptapg04@gmail.com>
2023-01-19 22:35:29 +0530

    jjjjjjjjjjjjj

--- src/main/java/Appium.java
+++ src/main/java/Appium.java
@@ -6,5 +6,10 @@ public void testA() {
 		System.out.println("MOBILE");
 		System.out.println("MOBILE");System.out.println("MOBILE");System.out.println("MOBILE");
 	}
+public void testB() {
+	
+	System.out.println("MOBILE");
+	System.out.println("MOBILE");System.out.println("MOBILE");System.out.println("MOBILE");
+}
 
 }
